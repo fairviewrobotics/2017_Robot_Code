f2fed895ec628cd311a39fc6633a172729269bc6
svirisov <svirisov01@bvsd.org>
2017-01-25 16:48:43 -0700

    gearoutake.py

---
+++ subsystems/gearoutake.py
@@ -0,0 +1,13 @@
+import wpilib
+
+from wpilib.command.subsystem import subsystem
+
+import robotmap
+
+class GearOutake(Subsystem)
+	
+	def __init__(self):
+		super().__init__('Gear Outake')
+		
+	#work in progress
+	#to be finished
