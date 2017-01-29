38292144f87774508b2048a2afc15637cb8df105
svirisov <svirisov01@bvsd.org>
2017-01-29 10:56:20 -0700

    Defined position

--- subsystems/gearoutake.py
+++ subsystems/gearoutake.py
@@ -8,7 +8,7 @@ class GearOutake(Subsystem)
 	def __init__(self):
 		super().__init__('Gear Outake')
 		self.servo = wpilib.Servo(robotmap.port.gearDoorID)
+		position = 0.0
 
 	def set(self, position):
 		self.servo.set(position)
-	
