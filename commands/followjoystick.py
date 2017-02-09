9f7c9bee1b232836646f8d2470701f59144dc6ff
Saurabh Totey <SaurabhTotey@gmail.com>
2017-02-08 17:04:04 -0700

    Added deadzone code for movement joystick

--- commands/followjoystick.py
+++ commands/followjoystick.py
@@ -2,7 +2,7 @@ from wpilib.command import Command
 import subsystems
 import robotmap
 import operatorinput as oi
-
+import math
 
 class FollowJoystick(Command):
 
@@ -11,11 +11,17 @@ class FollowJoystick(Command):
         self.requires(subsystems.driveTrain)
 
     def execute(self):
-        subsystems.driveTrain.set(
-            oi.joystick.getX(),
-            oi.joystick.getY(),
-            oi.joystick.getZ(),
-            0)
+        xAxis = oi.joystick.getX()
+        yAxis = oi.joystick.getY()
+        zAxis = oi.joystick.getZ()
+        distance = math.sqrt(math.pow(xAxis, 2) + math.pow(yAxis, 2))
+        if distance <= robotmap.speedsList.deadZoneRadius:
+            xAxis = 0
+            yAxis = 0
+        else:
+            xAxis = (xAxis - robotmap.speedsList.deadZoneRadius) / (1 - robotmap.speedsList.deadZoneRadius)
+            yAxis = (yAxis - robotmap.speedsList.deadZoneRadius) / (1 - robotmap.speedsList.deadZoneRadius)
+        subsystems.driveTrain.set(xAxis, yAxis, zAxis, 0)
 
     def end(self):
         subsystems.driveTrain.set(0, 0, 0, 0)
